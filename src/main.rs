4e100c1a484691d7e0c4dae185267d68eaceaee0
ytyaru <ytyaru@outlook.jp>
2019-06-01 15:06:21 +0900

    Range型追加

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
  * Rustでfor。
  * CreatedAt: 2019-06-01
  */
+use std::ops::Range;
+
 fn main() {
 //    let mut x = 0;
 //    for (x = 0; x < 10; x++) {
@@ -11,6 +13,7 @@ fn main() {
         println!("for {}", x);
     }
     for x in (0..10).rev() { println!("for {}", x); }
+//    for x in 9..-1 { println!("for {}", x); }
 
     let array = [1, 3, 5];
     for v in array.iter() { println!("array {}", v); }
@@ -18,6 +21,11 @@ fn main() {
     let names = vec!["Bob", "Frank", "Ferris"];
     for name in names.iter() { println!("{}", name); }
 
-//    for x in get_ten() { println!("for {}", x); } // 
+    for x in get_ten() { println!("for {}", x); }
 }
 //fn get_ten() { 0..10 } // error[E0308]: mismatched types    std::ops::Range
+//fn get_ten() -> i32 { 0..10 } // error[E0308]: mismatched types    std::ops::Range
+//fn get_ten() -> std::ops::Range { 0..10 } // error[E0107]: wrong number of type arguments: expected 1, found 0
+//fn get_ten() -> Range { 0..10 } // error[E0107]: wrong number of type arguments: expected 1, found 0
+fn get_ten() -> Range<i32> { 0..10 } 
+
