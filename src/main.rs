01db3017511d620f0c97f2df6ea9e58b078fd1c8
ytyaru <ytyaru@outlook.jp>
2019-06-01 14:37:52 +0900

    初回コミット

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+/*
+ * Rustでfor。
+ * CreatedAt: 2019-06-01
+ */
+fn main() {
+//    let mut x = 0;
+//    for (x = 0; x < 10; x++) {
+//        println!("for {}", x);
+//    }
+    for x in 0..10 {
+        println!("for {}", x);
+    }
+
+    let names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter() { println!("{}", name); }
+
+//    for x in get_ten() { println!("for {}", x); } // 
+}
+//fn get_ten() { 0..10 } // error[E0308]: mismatched types    std::ops::Range
